3aef7645a62411cff047566718041c06e6ec45b0
ivanleo94 <31164505+ivanleo94@users.noreply.github.com>
2018-01-25 15:59:08 -0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,26 @@
+var listOfWords = [
+  'football',
+  'basketball',
+  'baseball',
+  'soccer',
+  'hockey',
+  'tennis',
+  'lacrosse',
+  'wrestling',
+  'equestrian',
+  'track',
+  'field',
+  'rowing',
+  'diving',
+  'field',
+  'cheerleading',
+  'karate',
+  'mma',
+  'kung fu',
+  'golf',
+  'surfing',
+  'snowboarding'
+];
+
+// Export the list of words to other hangman script(s)
+module.exports = listOfWords;
